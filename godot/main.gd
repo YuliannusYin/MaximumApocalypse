0d6633f6b392719d6cab438c3db6fffdef543d6b
Sunburst <EulaliaYundtnhsjf@outlook.com>
2026-06-24 17:59:21 +0800

    feat(godot): 增加开始场景和游戏场景
    1. 新增game.tscn游戏场景文件
    2. 调整退出按钮布局并添加开始按钮点击场景切换逻辑
    3. 绑定开始按钮的点击信号到处理函数

--- godot/main.gd
+++ godot/main.gd
@@ -14,3 +14,10 @@ func _process(delta: float) -> void:
 func _on_quit_button_button_down() -> void:
 	pass # Replace with function body.	
 	get_tree().quit()
+
+
+func _on_start_button_button_down() -> void:
+	pass # Replace with function body.
+	#get_tree().change_scene_to_file("res://godot/game.tscn")
+	var scene_game = preload("res://godot/game.tscn")
+	get_tree().change_scene_to_packed(scene_game)
